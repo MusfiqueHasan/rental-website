889c88e8b3efd88ab78797e3286d088a8a61d787
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-06-25 17:49:42 +0600

    connected database

--- ServerSide/App.js
+++ ServerSide/App.js
@@ -1,8 +1,32 @@
+const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
+const DB = 'mongodb+srv://musfique:rentalweb@cluster0.rdfrd.mongodb.net/rental-db?retryWrites=true&w=majority';
+mongoose.connect(DB, {
+    useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:false
+    
+}).then( ()=>{
+    console.log(`connection succesful`);
+}).catch((err)=>{
+    console.log(`Error connection`);
+})
+
+
+const middleware = (req,res,next)=>{
+    console.log(`Hellow middleware`);
+    next();
+}
+
+
 app.get('/', (req,res) =>{
-    res.send(`Hellow World`);
+    res.send(`Hellow World this is home page`);
+});
+app.get('/about', middleware, (req,res) =>{
+    res.send(`Hellow World this is about page`);
+});
+app.get('/contact', (req,res) =>{
+    res.send(`Hellow World this is contact page`);
 });
 app.listen(3000, () => {
-    console.log(`Now server is listening at port 3000`);
+    console.log(`Now server is runnig at port 3000`);
 })
