ffe8e4761fb0df6d9d192ecf81358903448a6481
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-06-25 21:52:24 +0600

    schema added

--- ServerSide/App.js
+++ ServerSide/App.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv');
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
-dotenv.config({path:'./config.env'});
+dotenv.config({path:'./config.env'}); 
 require('./DB/Connection');
-
+const User =require('./model/UserSchema');
 
 const PORT = process.env.PORT;
 
@@ -26,5 +26,5 @@ app.get('/contact', (req,res) =>{
     res.send(`Hellow World this is contact page`);
 });
 app.listen(PORT, () => {
-    console.log(`Now server is runnig at port ${PORT}`);
+    console.log(`Now server is running at port ${PORT}`);
 })
