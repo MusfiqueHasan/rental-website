2525084c2605b810a0c33dd63984da047290c38c
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-06-27 18:05:27 +0600

    using asyn-await

--- ServerSide/router/Auth.js
+++ ServerSide/router/Auth.js
@@ -2,33 +2,34 @@ const express = require('express');
 const router = express.Router();
 require('../DB/Connection');
 const User = require('../model/UserSchema')
+
+
+
 router.get('/', (req,res) =>{
     res.send(`Hellow World this is home page from router`);
 });
 
-router.post('/register', (req, res)=>{
+router.post('/register', async (req, res)=>{
 
     const { name,email, password, cpassword }=req.body;
     if( !name ||!email ||! password || !cpassword){
         return res.status(422).json({error:`fill the required data`});
     }
+    try {
 
-    User.findOne({email:email})
-    .then((userExist) => {
-         if(userExist)
-         {return res.status(422).json({error:`Email already exist `});}
+            const userExist = await User.findOne({email:email});
+             if(userExist){
+                 return res.status(422).json({error:`Email already exist `});
+                }
+            const user = new User({ name, email, password, cpassword});
+          
+            await user.save();
+            res.status(201).json({message : `User registered sucssesfully`});
 
-         const user = new User({ name, email, password, cpassword})
-
-         user.save().then(()=>{
-             res.status(201).json({message:`user is registered`})
-         }).catch(()=>{
-             res.status(500).json({error:`user is not registered `})
-         })
-        
-    }).catch((err) => {
-        console.log(err);
-    });
+    } catch (err) {
+        console.log(err) 
+    }
+   
 
 });
 
