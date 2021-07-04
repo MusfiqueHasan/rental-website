f4d0f0d032b82e6696d6967aee85e8f7ef4a91c3
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-07-04 23:34:59 +0600

    search option added

--- ClientSide/src/components/Chittagong.js
+++ ClientSide/src/components/Chittagong.js
@@ -1,39 +1,82 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Card from './Card';
-// import './styles/App.css'
+import Map from '../components/image/map1.jpg'
+
 
 
 const Chittagong = () => {
 
+    const [input, setInput] = useState([]);
+    const [myInput, setMyInput] = useState([]);
+    const openNewWindow = event => {
+
+        event.preventDefault();
+        if (myInput !== null) {
+            setInput(myInput);
+
+        }
+
+    }
 
     return (
         <>
-         { Card.map((item) => {
-       const {image, name, price,description }= item;
-          return(
-            
-           <div className="card-sec">
-           <div className="card">
-                <div className="imag-sec">
-                    <img src={image} alt="" />
+                   
+                   <h1 className="card-title">Welcome to Chittagong</h1>
+
+            <form action="" className="d-flex justify-content-lg-center align-items-center flex-row w-100 mt-3 "
+                onSubmit={openNewWindow}
+            >
+
+                <div className="form-row">
+                    <input type="text" className="form-control mx-3 p-2" placeholder="Search Service..."
+
+                        onChange={event => setMyInput(event.target.value)}
+                    />
                 </div>
-                <div className="descrip">
-                    <h3>{name}</h3>
-                    <h4>price:{price}/-</h4>
-                    <p>{description}</p>
-                    <a href="#">view</a>
+                <div className="form-row">
+                    <input type="submit" value="Search" className="btn btn1 btn-lg btn-block mx-3 px-5" />
                 </div>
+
+
+            </form>
+
+            <section className="main-card">
+            <div className="card-block">
+                    
+                    {Card.filter(product => product.name.includes(input)).map((item) => {
+                        const { image, name, price, description } = item;
+                        return (
+        
+                            <div className="card-sec">
+                                <div className="card">
+                                    <div className="imag-sec">
+                                        <img src={image} alt="" />
+                                    </div>
+                                    <div className="descrip">
+                                        <h3>{name}</h3>
+                                        <h4>price:{price}/-</h4>
+                                        <p>{description}</p>
+                                        <a href="#">Booking</a>
+                                    </div>
+        
+                                </div>
+                            </div>
+        
+        
+                        )
+                    })}
+                        </div>
+                <div className="card-block2">
+                <img src={Map} alt="" />
+                </div>
+            </section>
                 
-            </div>
-           </div>
-            
-           
-          )
-        })} 
+
+
         </>
-       
+
     )
-   
+
 }
 
 export default Chittagong;
